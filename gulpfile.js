681e689ded1472439f935a379b103bd808ab08a8
pewho <pewhoo@gmail.com>
2016-01-29 11:47:18 +0100

    Add base app

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync');
+var sass        = require('gulp-sass');
+var sourcemaps  = require('gulp-sourcemaps');
+var reload      = browserSync.reload;
+
+var src = {
+    scss: 'app/scss/*.scss',
+    css:  'app/css',
+    html: 'app/*.html'
+};
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync({
+        server: "./app"
+    });
+
+    gulp.watch(src.scss, ['sass']);
+    gulp.watch(src.html).on('change', reload);
+});
+
+// Compile sass into CSS
+gulp.task('sass', function() {
+    return gulp.src(src.scss)
+        .pipe(sourcemaps.init())
+        .pipe(
+            sass({
+                outputStyle: 'compressed',
+                includePaths: ['node_modules/susy/sass']
+                }
+            )
+        )
+        .pipe(sourcemaps.write('maps'))
+        .pipe(gulp.dest(src.css))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('default', ['serve']);
